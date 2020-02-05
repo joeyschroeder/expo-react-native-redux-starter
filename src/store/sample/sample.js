2cf0b60e0904687dbfa87feb34b43432078851e7
Joey Schroeder <schrjos@gmail.com>
2020-02-05 16:22:58 -0600

    Rename "ducks" to "store"

--- src/store/sample/sample.js
+++ src/store/sample/sample.js
@@ -1,15 +1,19 @@
 import { createAction, handleActions } from 'redux-actions';
 
+// action types
 const UPDATE = 'sample/UPDATE';
 const RESET = 'sample/RESET';
 
+// action creators
 export const updateSampleData = createAction(UPDATE);
 export const resetSampleData = createAction(RESET);
 
+// default state
 export const DEFAULT_STATE = {
   data: 0
 };
 
+// reducer
 export const sampleReducer = handleActions(
   {
     [UPDATE]: state => ({ ...state, data: state.data + 1 }),
@@ -18,5 +22,8 @@ export const sampleReducer = handleActions(
   DEFAULT_STATE
 );
 
+// selectors
 export const selectSample = state => state.sample || DEFAULT_STATE;
 export const selectSampleData = state => selectSample(state).data || DEFAULT_STATE.data;
+
+// thunks
--- src/util/configure-store/configure-store.js
+++ src/util/configure-store/configure-store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 
-import { reducers } from '../../ducks/index';
+import { reducers } from '../../store/index';
 import thunk from 'redux-thunk';
 
 export const configureStore = () => createStore(reducers, applyMiddleware(thunk));
